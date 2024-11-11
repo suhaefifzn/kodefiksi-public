9bff0edbaae5681a7666122eef3e34533a09096b
suhaefifzn <suhaefi21@gmail.com>
2024-11-11 10:06:45 +0700

    update: google tag manager for cookie consent and my.css

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -124,16 +124,14 @@
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
 
-        // Set default consent values
-        gtag('consent', 'default', {
-          'ad_storage': 'denied',
-          'ad_user_data': 'denied',
-          'ad_personalization': 'denied',
-          'analytics_storage': 'denied'
-        });
-
         document.addEventListener("DOMContentLoaded", function() {
             if (!localStorage.getItem("cookieConsent")) {
+                gtag('consent', 'default', {
+                    'ad_storage': 'denied',
+                    'ad_user_data': 'denied',
+                    'ad_personalization': 'denied',
+                    'analytics_storage': 'denied'
+                });
                 document.getElementById("cookie-consent-banner").style.display = "block";
             } else {
                 activateAnalytics();
