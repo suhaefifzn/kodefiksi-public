1e86f18c3ab9231485a126934dcf346c006a01f2
suhaefifzn <suhaefi21@gmail.com>
2024-11-11 09:47:23 +0700

    update: google tag manager for cookie consent and my.css

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -127,6 +127,8 @@
         // Set default consent values
         gtag('consent', 'default', {
           'ad_storage': 'denied',
+          'ad_user_data': 'denied',
+          'ad_personalization': 'denied',
           'analytics_storage': 'denied'
         });
 
